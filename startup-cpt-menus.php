7c9213d90866f3a9df418c337e11eee99e8d63b8
yozz <yozz.online@gmail.com>
2015-11-24 09:29:46 -0500

    Security

--- startup-cpt-menus.php
+++ startup-cpt-menus.php
@@ -7,6 +7,11 @@ Version: 0.2.0
 Text Domain: startup-cpt-menus
 */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
 //GitHub Plugin Updater
 function startup_reloaded_menus_updater() {
 	include_once 'lib/updater.php';
